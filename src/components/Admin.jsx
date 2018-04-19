28025b3fd160b8366a9b570c9b62b6eafb36f072
Guest User <Guest@epicodus-79.local>
2018-04-19 08:58:52 -0700

    center content

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,18 +4,20 @@ const styles = {
   grid: {
     display: "grid",
     width: "700px",
+    height: "700px",
     gridTemplateColumns: "repeat(3, 1fr)",
     gridTemplateRows: "repeat(3, 1fr)",
     textAlign: "center",
     margin: "auto",
     marginTop: "100px",
-    gridGap: "17px"
+    gridGap: "1px"
   },
   cell: {
     width: "200px",
     height: "200px",
     border: "1px solid black",
-    lineHeight: "200px"
+    lineHeight: "200px",
+    margin: "auto"
   }
 }
 
@@ -59,15 +61,15 @@ function Admin(){
         `}
       </style>
       <div id="grid" style={styles.grid}>
-        <div id="c1" style={styles.cell}>1</div>
-        <div id="c2" style={styles.cell}>2</div>
-        <div id="c3" style={styles.cell}>3</div>
-        <div id="c4" style={styles.cell}>4</div>
-        <div id="c5" style={styles.cell}>5</div>
-        <div id="c6" style={styles.cell}>6</div>
-        <div id="c7" style={styles.cell}>7</div>
-        <div id="c8" style={styles.cell}>8</div>
-        <div id="c9" style={styles.cell}>9</div>
+        <div id="c1" style={styles.cell}>x</div>
+        <div id="c2" style={styles.cell}>x</div>
+        <div id="c3" style={styles.cell}>x</div>
+        <div id="c4" style={styles.cell}>x</div>
+        <div id="c5" style={styles.cell}>x</div>
+        <div id="c6" style={styles.cell}>x</div>
+        <div id="c7" style={styles.cell}>x</div>
+        <div id="c8" style={styles.cell}>x</div>
+        <div id="c9" style={styles.cell}>x</div>
       </div>
     </div>
 
--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Error404(props){
   console.log(props);
